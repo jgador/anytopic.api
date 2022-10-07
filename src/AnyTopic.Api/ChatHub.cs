20397ab7caa21acef49a4352b3c48bb11eefe776
Jesse Gador <jessegador@outlook.com>
2022-10-07 11:06:07 +0800

    Able to communicate with postman

---
+++ src/AnyTopic.Api/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AnyTopic.Api
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ src/AnyTopic.Api/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace AnyTopic.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+
+            services.AddControllers();
+
+            services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                endpoints.MapHub<ChatHub>("/chatHub");
+            });
+        }
+    }
+}
