f47046e11039ba095c61aa255823f7d2d3287098
Jesse Gador <jessegador@outlook.com>
2022-10-08 13:16:44 +0800

    Separate api and web host

---
+++ src/AnyTopic.Api.old/Program.cs
@@ -0,0 +1,19 @@
+//namespace AnyTopic.Api
+//{
+//    public class Program
+//    {
+//        public static async Task Main(string[] args)
+//        {
+//            await CreateHostBuilder(args)
+//                .Build()
+//                .RunAsync();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .ConfigureWebHostDefaults(webHostBuilder =>
+//                {
+//                    webHostBuilder.UseStartup<Startup>();
+//                });
+//    }
+//}
