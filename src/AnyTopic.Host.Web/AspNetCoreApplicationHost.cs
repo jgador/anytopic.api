52b111340ce8da98fd5d29903f13f4afcca360c2
Jesse Gador <jessegador@outlook.com>
2022-10-08 13:26:02 +0800

    Delete api csproj

--- src/AnyTopic.Host.Web/AspNetCoreApplicationHost.cs
+++ src/AnyTopic.Host.Web/AspNetCoreApplicationHost.cs
@@ -1,31 +1,31 @@
-﻿using AnyTopic.Api;
-using AnyTopic.Api.Hosting;
+﻿//using AnyTopic.Api;
+//using AnyTopic.Api.Hosting;
 
-namespace AnyTopic.Host.Web
-{
-    public class AspNetCoreApplicationHost : AnyTopicApiHost
-    {
-        protected override void ConfigureServices(HostBuilderContext context, IServiceCollection services)
-        {
-            services.AddSignalR();
+//namespace AnyTopic.Host.Web
+//{
+//    public class AspNetCoreApplicationHost : AnyTopicApiHost
+//    {
+//        protected override void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+//        {
+//            services.AddSignalR();
 
-            services.AddControllers();
+//            services.AddControllers();
 
-            services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-        }
+//            services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+//        }
 
-        protected override void Configure(WebHostBuilderContext context, IApplicationBuilder app)
-        {
-            app.UseRouting();
+//        protected override void Configure(WebHostBuilderContext context, IApplicationBuilder app)
+//        {
+//            app.UseRouting();
 
-            app.UseCors();
+//            app.UseCors();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllers();
 
-                endpoints.MapHub<ChatHub>("/chatHub");
-            });
-        }
-    }
-}
+//                endpoints.MapHub<ChatHub>("/chatHub");
+//            });
+//        }
+//    }
+//}
